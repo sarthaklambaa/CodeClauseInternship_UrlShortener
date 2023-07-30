a1305657862e83053bf3a0052eb961c8f3514a75
sarthaklambaa <samlamba29@gmail.com>
2023-07-31 00:59:57 +0530

    Created Database and basic HTML file

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
+import os
+from datetime import datetime
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# Creating Database
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///url.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class ShortUrls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    short_id = db.Column(db.String(20), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
+
 
 @app.route('/')
 def index():

04ff87f80a31547203b43d0aea7d9e6309ff89af
sarthaklambaa <samlamba29@gmail.com>
2023-08-01 23:31:38 +0530

    Fixed if and else statement

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Creating SQLite Database
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///url.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///url.db" #url is name
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -17,14 +17,22 @@ class ShortUrls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(500), nullable=False)
     short_id = db.Column(db.String(20), nullable=False, unique=True)
-    created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
+
+    def __init__(self, long, short):
+      self.long = long
+      self.short = short
+
+with app.app_context():
+    db.create_all()
 
 # Defining the index route
 @app.route('/', methods = ['POST','GET'])
 def index():
    if request.method == 'POST':
       url_received = request.form['url']
-      print(url_received)
+      return url_received
+   else:
+      return render_template('index.html')
 
 # Running the Flask application
 if __name__ == '__main__':
